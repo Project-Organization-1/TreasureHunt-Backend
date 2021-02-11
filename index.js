3796bc5356deda0a7e1930bdaebab61afa4404bf
siddheshkothadi <realsiddheshko@gmail.com>
2021-02-11 11:50:20 +0530

    added cors

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter = require("./routes/user");
 
-
 const App = express();
 
 dotenv.config();
 const uri = process.env.ATLAS_URI;
+
+var cors = require('cors')
+
+App.use(cors())
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
@@ -23,8 +26,18 @@ mongoose.connect(uri, {
     console.log(err);
 });
 
+App.use(cors(
+    {
+      origin: "http://localhost:3000", // allow to server to accept request from different origin
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true // allow session cookie from browser to pass through
+    }
+  ));
+
 App.use('/user', userRouter);
 
 App.listen(5000, () => {
     console.log("Hello World");
 });
+
+module.exports = App;
