2268dc2f843db008a90758ca8ea90f87e8938b04
Kaustubh <kaustubhdeore2109@gmail.com>
2021-02-12 12:31:26 +0530

    Added get route for login

--- routes/user.js
+++ routes/user.js
@@ -7,15 +7,34 @@ router.route('/').get((req,res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
-  const groupId = req.body.groupId;
-  const email = req.body.email;
+router
+.route('/login/Q').get( async (req, res) => {
+  const groupId = req.params.gpId;
+  const email = req.params.email;
+  console.log(groupId);
+  console.log(email);
 
-  const newUser = new User({groupId,email});
+  const group = await User.find({groupId: groupId});
+  if(!group){
+    return res.statusMessage('Group not found!')
+  }
+  console.log(group);
+})
 
-  newUser.save()
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
+// router.route('/').get((req, res) => {
+//   Note.find({googleId : req.params.googleId})
+//     .then(note => res.status(200).json(note))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+// router.route('/add').post((req, res) => {
+//   const groupId = req.body.groupId;
+//   const email = req.body.email;
+
+//   const newUser = new User({groupId,email});
+
+//   newUser.save()
+//         .then(() => res.json('User added!'))
+//         .catch(err => res.status(400).json('Error: ' + err));
+// });
 
 module.exports = router;
